e546887aecc5d8ce7528da42b04b39baebde05a8
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-03 13:14:29 -0500

    I did it dad!

---
+++ Grades/Grade.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Grades
+{
+    partial class frmGrades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.btnGetPercentage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(13, 13);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(150, 13);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "Enter your grade number here:";
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(13, 117);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(99, 13);
+            this.lblPercentage.TabIndex = 1;
+            this.lblPercentage.Text = "Your percentage is:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(16, 45);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // btnGetPercentage
+            // 
+            this.btnGetPercentage.Location = new System.Drawing.Point(16, 213);
+            this.btnGetPercentage.Name = "btnGetPercentage";
+            this.btnGetPercentage.Size = new System.Drawing.Size(96, 26);
+            this.btnGetPercentage.TabIndex = 3;
+            this.btnGetPercentage.Text = "Get Percentage";
+            this.btnGetPercentage.UseVisualStyleBackColor = true;
+            this.btnGetPercentage.Click += new System.EventHandler(this.btnGetPercentage_Click);
+            // 
+            // frmGrades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGetPercentage);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "frmGrades";
+            this.Text = "Grade Percentages";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblPercentage;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Button btnGetPercentage;
+    }
+}
+
---
+++ Grades/Grade.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * Created by: Alex Mathias
+ * Created on: 03-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 4-04
+ * This program displays a number grade conversion to percentage
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grades
+{
+    public partial class frmGrades : Form
+    {
+        public string PercentageMaker(string letter)
+        {
+            // Conversion process
+
+            string mark = "Nothing there.";
+
+            if (letter == "4+")
+            {
+                mark = "Your percentage is: 95%";
+            }
+            else if(letter == "4")
+            {
+                mark = ("Your percentage is 90%");
+            }
+            else if (letter == "4-")
+            {
+                mark = ("Your percentage is 85%");
+            }
+            else if (letter == "3+")
+            {
+                mark = ("Your percentage is 77%");
+            }
+            else if (letter == "3")
+            {
+                mark = ("Your percentage is 75%");
+            }
+            else if (letter == "3-")
+            {
+                mark = ("Your percentage is 72%");
+            }
+            else if (letter == "2+")
+            {
+                mark = ("Your percentage is 67%");
+            }
+            else if (letter == "2")
+            {
+                mark = ("Your percentage is 65%");
+            }
+            else if (letter == "2-")
+            {
+                mark = ("Your percentage is 62%");
+            }
+            else if (letter == "1+")
+            {
+                mark = ("Your percentage is 57%");
+            }
+            else if (letter == "1")
+            {
+                mark = ("Your percentage is 55%");
+            }
+            else if (letter == "1-")
+            {
+                mark = ("Your percentage is 52%");
+            }
+            else if (letter == "R")
+            {
+                mark = ("Your percentage is 30%");
+            }
+            else if (letter == "NE")
+            {
+                mark = ("Your percentage is 0%");
+            }
+
+            return mark;
+        }
+
+        public frmGrades()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetPercentage_Click(object sender, EventArgs e)
+        {
+            // Displaying the Percentage
+
+            string mark;
+
+            //Input 
+            mark = this.txtNumber.Text;
+
+            // Process & output
+            this.lblPercentage.Text = PercentageMaker(mark);
+        }
+    }
+}
